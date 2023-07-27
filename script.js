d553fbb88c49bd7760c5a5a6aaefeda9bbef0c14
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2023-07-27 18:31:16 +0500

    indexOf method

--- script.js
+++ script.js
@@ -11,12 +11,20 @@ numbersArray.pop()
 numbersArray.pop()
 console.log(numbersArray)
 
-// // shift method (used to remove element at the start of array)
-// numbersArray.shift();
-// console.log(numbersArray);
+// shift method (used to remove element at the start of array)
+numbersArray.shift();
+console.log(numbersArray);
 
-// // UnShift method (used to Add element at the start of array)
-// numbersArray.unshift(2)
-// numbersArray.unshift(0)
-// console.log(numbersArray);
+// UnShift method (used to Add element at the start of array)
+numbersArray.unshift(2)
+numbersArray.unshift(0)
+console.log(numbersArray);
+
+//indexOf method (ye method jab pehla occurance find hjyga to usi ka index dega agar us occurence ke bad koi occurence hui to wo ye include nhi krega us k lie hamy , lga kr dubara se search start krwani hogi )
+
+const animalNames = [ 'lion' , 'tiger', 'horse' , 'hen' , 'lion', 'hen'];
+console.log(animalNames.indexOf('lion'));
+console.log(animalNames.indexOf('lion',1));
+
+
 
