86137fe8e0e0ca6c2af5882f175dbd5847ce3be1
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2023-07-27 18:16:24 +0500

    shift and unshift method

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ numbersArray.push(12);
 console.log(numbersArray);
 numbersArray.push(14,16,18,20);
 console.log(numbersArray);
+
+// shift method (used to remove element at the start of array)
+numbersArray.shift();
+console.log(numbersArray);
+
+// UnShift method (used to Add element at the start of array)
+numbersArray.unshift(2)
+numbersArray.unshift(0)
+console.log(numbersArray);
+
