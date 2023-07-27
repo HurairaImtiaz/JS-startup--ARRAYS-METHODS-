e58a52bbe5b8fed8f9c1d77f2feccd8b9decb0b7
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2023-07-27 18:03:41 +0500

    push method

---
+++ script.js
@@ -0,0 +1,7 @@
+var numbersArray = [2,4,6,8,10];
+console.log(numbersArray);
+// push method (used to add element in the end of array)
+numbersArray.push(12);
+console.log(numbersArray);
+numbersArray.push(14,16,18,20);
+console.log(numbersArray);
